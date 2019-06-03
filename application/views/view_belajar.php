95b222751957382452d09981938d6c751f3ff5da
Revtm <kykylizaro98@gmail.com>
2019-06-03 09:18:49 +0700

    update index

--- application/views/view_belajar.php
+++ application/views/view_belajar.php
@@ -8,7 +8,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
   <body>
     <div class = "tengah" style="text-align: center">
-      <h1> Selamat datang <?php echo $nama ?> di Project <b> Revtm 1.0 </b> </h1>
+      <h1> Selamat datang <?php echo $nama ?> di Project <b> Revtm 0.0.1 </b> </h1>
     </br>
     <p> Ini adalah project sederhana untuk mempelajari bahasa pemrograman dan framework</p>
     <p> <b>MySQL, PHP, CodeIgniter, HTML, JavaScript, CSS,</b> & <b>Bootstrap</b> </p>
